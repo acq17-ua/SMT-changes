8a6ec038dc374bc82011d7510ddc2c7bd73651c9
acq17-ua <acq17@gcloud.ua.es>
2025-05-12 02:00:21 +0200

    creates images properly, overall just makes addition with is probably wrong, tokens are not clearly viewed

--- interface.py
+++ interface.py
@@ -2,32 +2,86 @@ from smt_trainer import SMT_Trainer
 import gradio as gr
 import torch
 import numpy as np
+from numpy.typing import ArrayLike
 from math import sqrt
 import cv2
+from cv2.typing import MatLike
 
-CA_layers = []
 
-colors = [  (0.0,   0.0,   0.0),
-			(0.0,   0.0,   0.0),
-			(0.0,   0.0,   0.0),
-			(0.0,   0.0,   0.0),
-			(0.0,   0.0,   0.0),
-			(0.0,   0.0,   0.0),
-			(0.0,   0.0,   0.0),
-			(0.0,   0.0,   0.0),
+CA_layers = list()
+
+colors = [  (128, 0, 0),
+			(128, 64, 0),
+			(128, 128, 0),
+			(0, 128, 0),
+			(0, 128, 128),
+			(0, 64, 128),
+			(0, 0, 128),
+			(128, 0, 128),
+			(128, 0, 0)
 			]
 
 def contrast(elem):
 	return elem!=0
 
+def overlay(background:np.ndarray, overlay:np.ndarray, alpha=1):
+	"""
+	:param background: BGR image (np.uint8)
+	:param overlay: BGRA image (np.uint8)
+	:param alpha: Transparency of overlay over background
+	
+	returns BGR image of combined images (np.float32)
+	"""
+
+	# add alpha channel to background
+	background = np.concatenate([background, np.full([*background.shape[:2], 1], 1.0)], axis=-1 )
+
+	# normalize overlay alpha channel from 0-255 to 0.-1.
+	alpha_background = 1.0
+	alpha_overlay = overlay[:,:,3] / 255.0 * alpha
+
+	for channel in range(3):
+
+		background[:,:,channel] = 	alpha_overlay * overlay[:,:,channel] + \
+									alpha_background * background[:,:,channel] * ( 1 - alpha_overlay )
+
+	background[:,:,3] = ( 1 - ( 1 - alpha_overlay ) * ( 1 - alpha_background ) ) * 255
+
+	# ignore alpha channel because gradio doesnt care
+	# also divide by 255 because somehow it needs a float image even though it gives int images
+	return (background[:,:,:3]/255.0).astype(np.float32)
+
+def generate_CA_images(token_idx, image):
+
+	global CA_layers
+
+	CA_final_images = []
+
+	# resize to fit input image (value from 0-1)
+	masks = [ cv2.resize(CA_layers[layer_idx][token_idx],
+							interpolation=cv2.INTER_NEAREST,
+							dsize=(image.shape[1], image.shape[0])) for layer_idx in range(0, len(CA_layers)) ]
+
+	# (value from 0-255)
+	masks = [ (mask*255.0) for mask in masks ]
+	masks = np.round(masks).astype(np.uint8)
+
+	# add singleton dimension as channel
+	masks = [ np.expand_dims(mask, axis=-1) for mask in masks ]
+
+	# base color + transparency mask = BGRA
+	CA_token = [ np.concatenate((	np.full(shape=image.shape, fill_value=colors[i]), 
+							  		masks[i]), 
+									axis=-1) for i in range(9) ]                  
+
+	# attention layers individually overlaid with input image
+	CA_final_images = [ overlay(image, ca) for ca in CA_token ]
+
+	return CA_final_images
+
 def make_predictions(checkpoint, input_image):
 
 	global CA_layers
-	global CA_final_images
-	global input_image_h
-	global input_image_w
-
-	#print(f"type of checkpoint: {type(checkpoint)} of image: {type(input_image)}")
 
 	model_wrapper = SMT_Trainer.load_from_checkpoint(checkpoint)
 
@@ -37,8 +91,6 @@ def make_predictions(checkpoint, input_image):
 
 	input_image = input_image.to(torch.float32)
 
-	#print(f"initial input shape: {input_image.shape}")
-
 	# width / height
 	aspect_ratio = input_image.shape[3]/input_image.shape[2]
 
@@ -46,19 +98,12 @@ def make_predictions(checkpoint, input_image):
 	#   extracted features is FLAT input_image shape divided by 16
 	predicted_seq, predictions = model_wrapper.model.predict(input_image)
 
-	#print(f"initial CA shape: {predictions.cross_attentions[0].shape}")
-	#print(f"seq len: {len(predicted_seq)}")
-
 	# seq_len | reduced_h * reduced_w
 	CA_layers = [ ca_layer.squeeze() for ca_layer in predictions.cross_attentions ]
 	
-	input_image_h, input_image_w = CA_layers[0].shape[0], CA_layers[0].shape[1]
-
 	seq_len = CA_layers[0].shape[0]
 	att_w = round(sqrt(CA_layers[0].shape[1] * aspect_ratio))
 	att_h = round(sqrt(CA_layers[0].shape[1] / aspect_ratio))
-
-	print(f"image dims: {input_image.shape}")
 
 	# make the attention 2-D
 	CA_layers = [ att.reshape( seq_len, att_h, att_w ) for att in CA_layers ]
@@ -67,35 +112,22 @@ def make_predictions(checkpoint, input_image):
 	CA_layers = [ att.cpu().detach().numpy() for att in CA_layers ]
 	# ^^^ we store this, then generate the actual images to display ONLY whenever the token slider is moved
 
-	# deberia generar las imagenes del primer token
-	CA_final_images = [ cv2.resize(CA_layers[layer_idx][0],
-								   interpolation=cv2.INTER_NEAREST,
-								   dsize=(input_image.shape[3], input_image.shape[2])) for layer_idx in range(len(CA_layers)) ]
+	## overlay all of them as overall attention
+	overall = np.empty(CA_layers[0].shape)
+	for ca in CA_layers:
+		overall += ca
 
-	CA_final_images.append(np.zeros((256,256)))
+	## normalize
+	overall /= np.max(overall)
 
-	## overlay all of them
+	CA_layers.append(overall)
 
-	"""     
-	overall_attention = np.zeros(input_image.shape[3], input_image.shape[2])
-	for i in range(len(CA_final_images)-1):
-
-		overall_attention += cv2.addWeighted(cv2.bitwise_not(CA_final_images[i]),0.1,
-											cv2.bitwise_not(CA_final_images[i+1]), 0.1 )
-	"""
-	#print(f"len ca_layers: {len(CA_layers)}")
-	#print(f"final images len: {len(CA_final_images)}")
-	#print(f"shape after resize: {CA_final_images[0].shape}")
-
-	return predicted_seq
+	return "\t".join(predicted_seq)
 
 def define_interface():
 
-	CA_tabs = []
 	predicted_seq_output=None
-	checkpoint_input=None
 	image_input=None
-	predict_button = None
 
 	with gr.Blocks() as page:
 
@@ -114,96 +146,49 @@ def define_interface():
 							outputs=[predicted_seq_output],
 							flagging_mode='never')
 				
-
 			with gr.Column(scale=2):
 
-				def generate_CA_images(token_idx, image):
+				token_slider = gr.Slider(minimum=0, maximum=0, step=1, 
+							 			label="Pick a token", 
+										info="Submit a sample first")
 
-					if image_input is None:
-						return
+				# modifica el slider cuando se hace una nueva prediccion
+				predicted_seq_output.change(fn=lambda prediction : gr.Slider(minimum=0, maximum=len(prediction.split("\t")), step=1, 
+																			label="Pick a token",
+																			info="Select a predicted token to visualize the attention it pays in the input sample"),
+											inputs=predicted_seq_output, 
+											outputs=token_slider)
 
-					CA_final_images = []
+				# genera las imagenes cada vez que se mueve el slider
+				@gr.render( inputs	=[predicted_seq_output, token_slider, image_input], 
+			   				triggers=[token_slider.release] )
+				def render_images_display(prediction, slider, image):
 
-					# resize to fit input image
-					masks = [ cv2.resize(CA_layers[layer_idx][token_idx],
-											interpolation=cv2.INTER_NEAREST,
-											dsize=(image.shape[1], image.shape[0])) for layer_idx in range(len(CA_layers)) ]
+					if len(prediction) != 0:
 
-					# add channels dimension
-					masks = [ np.expand_dims(mask, axis=-1) for mask in masks ]
+						print(f"triggered! (value={slider})")
 
-					# non-zero = 1, strength of attention will be shown through transparency
-					masks = [ np.apply_along_axis(contrast, 2, ca) for ca in masks ]
+						images = generate_CA_images(slider, image)
 
-					# base color + transparency mask = RGBA
-					CA_token = [ np.concatenate((np.full(shape=image.shape, fill_value=colors[i]), masks[i]), axis=-1) for i in range(8) ]                  
+						gr.Markdown(value="## Contents of the Cross-Attention layers")
 
-					# individual layers overlaid with input image
-					CA_final_images = [ cv2.addWeighted(np.concatenate((image, 
-														 				np.ones((image.shape[0], image.shape[1], 1))), 
-																		axis=-1), 
-														1, 
-														ca, 
-														0.4, 
-														0) for ca in CA_token ] 
+						with gr.Tab(f"Overall"):
+							gr.Image(value=images[8])
 
-					# overall view
-					overall = np.concatenate((image, 
-											np.ones((image.shape[0], image.shape[1], 1))), 
-											axis=-1)
-					
-					for ca in CA_final_images:
-						overall = cv2.addWeighted(overall, 1, ca, 0.125, 0)
-
-					CA_final_images.append(overall)
-
-					cv2.imshow("image", CA_final_images[8])
-
-					with gr.Tab(f"Overall"):
-						gr.Image(value=CA_final_images[8])
-
-					for i in range(8):  
-						with gr.Tab(f"Layer {i+1}") as tab:
-							gr.Image(value=CA_final_images[i])
-							CA_tabs.append(tab)
+						for i in range(8):  
+							with gr.Tab(f"Layer {i+1}") as tab:
+								gr.Image(value=images[i])
 					return
-
-				@gr.render(predicted_seq_output)
-				def render_knobs(predicted_seq):
-
-					print(f"rendering textbox ca_layer")
-
-					if( predicted_seq is not None and len(predicted_seq)==0 ):
-						print(f"textbox is empty ca_layer")
-						return
-
-					gr.Markdown(value="## Contents of the Cross-Attention layers")
-
-					with gr.Tab(f"Overall"):
-						gr.Image(interactive=False)
-
-					for i in range(8):  
-						with gr.Tab(f"Layer {i+1}") as tab:
-							gr.Image(value=CA_final_images[i])
-							CA_tabs.append(tab)
-
-					if( predicted_seq is not None and len(predicted_seq)==0 ):
-						print(f"textbox is empty slider")
-						return
-					
-					slider = gr.Slider(minimum=0,
-										maximum=CA_layers[0].shape[0],
-										label="Pick a token",
-										info = "Pick a token to see the attention it paid to each pixel in the image",
-										step=1)
-
-					slider.release(generate_CA_images, inputs=[slider, image_input])
-
-				#@gr.render(inputs=[token_slider, image_input], triggers=[token_slider.release])
-
 	return page
 
 if __name__=="__main__":
 	page = define_interface()
 	page.launch(share=False)
 
+'''
+with gr.Blocks() as demo: 
+	radio = gr.Radio([1, 2, 4], label="Set the value of the number") 
+	number = gr.Number(value=2, interactive=True) 
+	radio.change(fn=lambda value: gr.update(value=value), inputs=radio, outputs=number) 
+demo.launch()
+'''
